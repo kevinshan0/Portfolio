b72766fdc6fc26156a1af1fc570be58a4f470c35
Kevin Shan <kevinshan2017@gmail.com>
2024-04-04 21:20:48 -0400

    finished Info Component

--- src/lib/Info.svelte
+++ src/lib/Info.svelte
@@ -1,8 +1,44 @@
 <script lang="ts">
+    import { onMount } from "svelte";
+
+    let allTextDivs: NodeListOf<HTMLElement>;
     let infoAnchor: HTMLAnchorElement;
     let contactAnchor: HTMLAnchorElement;
 
-    function hoverAction(event: Event) {
+    async function onMountAnimation(): Promise<void> {
+        allTextDivs = document.querySelectorAll(".text");
+        let allTextArray = Array.from(allTextDivs).map((element) => element.innerText);
+        
+        allTextDivs.forEach(element => element.innerText = "");
+
+        for (const [index, element] of allTextDivs.entries()) {
+            await loadText(element, index);
+        }
+
+        function loadText(element: HTMLElement, index: number): Promise<void> {
+            return new Promise((resolve) => {
+                setTimeout(resolve, 120);
+
+                let innerTextArray = allTextArray[index];
+                let replacement = "";
+                let counter = 0;
+
+                function shuffle(): void {
+                    element.innerText = replacement;
+                    replacement += innerTextArray[counter];
+                    counter++;
+
+                    if (counter == innerTextArray.length + 1) {
+                        clearInterval(interval);
+                    }
+                }
+
+                let interval = setInterval(shuffle, 50);
+            })
+        }
+    }
+
+    function hoverAction(event: Event): void {
         let element = event.target as HTMLAnchorElement;
         let text = (element === infoAnchor) ? "INFO" : "CONTACT";
         let counter = 0;
@@ -69,36 +105,38 @@
             counter++;
 
             if (counter == text.length + 1) clearInterval(interval);
-            console.log()
         }
 
         let interval = setInterval(shuffle, 50);
     }
+
+    onMount(onMountAnimation);
 </script>
 
 <div class="container">
     <div class="inline">
-        <div>KEVIN SHAN</div>
-        <div>*</div>
+        <div class="text">KEVIN SHAN</div>
+        <div class="text">THE CREATOR</div>
     </div>
     <div class="inline">
-        <div>FULL STACK</div>
-        <div>DEV + DESIGN</div>
+        <div class="text">DEVELOPMENT</div>
+        <div class="text">+</div>
+        <div class="text">DESIGN</div>
     </div>
     <div class="inline">
-        <div>ANDOVER</div>
-        <div>AMHERST</div>
+        <div class="text">ANDOVER</div>
+        <div class="text">AMHERST</div>
     </div>
     <div class="inline space"></div>
     <div class="inline">
-        <div><a on:mouseenter={hoverAction} bind:this={infoAnchor} href="/info">INFO</a></div>
-        <div><a on:mouseenter={hoverAction} bind:this={contactAnchor} href="/contact">CONTACT</a></div>
+        <div><a on:mouseenter={hoverAction} bind:this={infoAnchor} href="/info" class="text">INFO</a></div>
+        <div><a on:mouseenter={hoverAction} bind:this={contactAnchor} href="/contact" class="text">CONTACT</a></div>
     </div>
 </div>
 
 <style>
     .container {
-        width: 300px;
+        width: 400px;
         height: fit-content;
         display: block;
     }
@@ -110,5 +148,10 @@
 
     .space {
         height: 1em;
+    }
+
+    a {
+        text-decoration: none;
+        color: black;
     }
 </style>
