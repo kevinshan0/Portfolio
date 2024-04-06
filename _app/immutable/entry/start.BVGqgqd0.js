78d08625b2d9058d4f6af6e09a9d0746d82a1360
Kevin Shan <kevinshan2017@gmail.com>
2024-04-06 11:03:13 -0400

    Updates

---
+++ _app/immutable/entry/start.BVGqgqd0.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.1tz8qYKh.js";export{t as start};
--- src/routes/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-    import Info from "$lib/Info.svelte";
-    import Showcase from "$lib/Showcase.svelte";
-    import ScrollNote from "$lib/ScrollNote.svelte";
-</script>
-
-<Info />
-<ScrollNote />
-<Showcase />
